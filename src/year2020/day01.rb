9eb0229ef7f328d9f6032d59cff905a950f214a3
Frédéric Boivin <frederic.boivin@shopify.com>
2020-12-01 18:34:44 -0800

    Bring day1 from old repo

---
+++ src/year2020/day01.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+module Year2020
+  class Day01
+    def part1(input)
+      data = format_input(input)
+
+      hash = {}
+      first = nil
+      second = nil
+
+      data.each do |row|
+        first = row
+        complement = 2020 - first
+
+        if hash[complement]
+          second = complement
+          break
+        end
+
+        hash[first] = complement
+      end
+
+      return first * second
+    end
+
+    def part2(input)
+      data = format_input(input)
+      set = data.combination(3).to_a
+
+      set.each do |perm|
+        if perm.sum == 2020
+          return perm.inject(:*)
+        end
+      end
+    end
+
+    def format_input(input)
+      data = input.split.map(&:to_i)
+    end
+  end
+end
